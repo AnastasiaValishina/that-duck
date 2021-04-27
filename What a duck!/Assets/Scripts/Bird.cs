f55f9bccf58b1d7e53cd0cc60089acf16b08738f
AnastasiaValishina <azhidenova@gmail.com>
2021-04-28 00:13:14 +0200

    project upload

---
+++ What a duck!/Assets/Scripts/Bird.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    [SerializeField] float speed = 4f;
+
+    Vector2 direction;
+    float xPos;
+    float yPos;
+
+    void Start()
+    {
+        xPos = Random.Range(-1f, 1f);
+        if (xPos < 0)
+        {
+            transform.localScale = new Vector2(1, 1);
+        }
+        else 
+        {
+            transform.localScale = new Vector2(-1, 1);
+        }
+        SetDirection();
+    }
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+
+    void SetDirection()
+    {
+        yPos = Random.Range(0.2f, 1f);
+        direction = new Vector2(xPos, yPos).normalized;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "RightBorder")
+        {
+            xPos = Random.Range(-1f, 0.2f);
+            transform.localScale = new Vector2(1, 1);
+            SetDirection();
+        }
+        if (collision.tag == "LeftBorder")
+        {
+            xPos = Random.Range(0.2f, 1f);
+            transform.localScale = new Vector2(-1, 1);
+            SetDirection();
+        }
+    }
+}
---
+++ What a duck!/Assets/Scripts/BirdSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdSpawner : MonoBehaviour
+{
+    [SerializeField] Bird birdPrefab;
+    //   [SerializeField] List<GameObject>;
+
+    bool isActive = true;
+    IEnumerator Start()
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            StartCoroutine(SpawnBird());
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnBird()
+    {
+        float xPos = Random.Range(-2, 2);
+        Vector2 birdPosition = new Vector2(xPos, -2);
+        Instantiate(birdPrefab, birdPosition, Quaternion.identity);
+        yield return new WaitForSeconds(1f);
+    }
+
+}
