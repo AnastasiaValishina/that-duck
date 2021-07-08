d765f319ff3fdc93d4853a4977ac1e52440a9cba
AnastasiaValishina <azhidenova@gmail.com>
2021-07-08 23:29:15 +0200

    refactoring

--- What a duck!/Assets/Scripts/Gun.cs
+++ What a duck!/Assets/Scripts/Gun.cs
@@ -1,55 +1,32 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Gun : MonoBehaviour
 {
     [SerializeField] Bullet bulletPrefab;
     [SerializeField] GameObject gun;
     [SerializeField] int bulletAmountFull = 5;
-    [SerializeField] float coolDownTime = 4f;
+    public float coolDownTime = 4f;
     [SerializeField] float timeBetweenShots = 0.5f;
 
-    [Header("UI")]
-    [SerializeField] Text bulletAmountText;
-    [SerializeField] Image coolDownImage;
+    public int bulletAmount;
+    public bool coolingDown = false;
 
-    Button shootButton;
-    int bulletAmount;
     float timeStamp;
-    bool coolingDown = false;
 
     void Start()
     {
-        shootButton = GetComponent<Button>();
-        shootButton.onClick.AddListener(ButtonClicked);
         bulletAmount = bulletAmountFull;
-        bulletAmountText.text = bulletAmount.ToString();
-        coolDownImage.fillAmount = 0f;
     }
 
-    void Update()
-    {
-        if (coolingDown)
-        {
-            coolDownImage.fillAmount -= 1.0f / coolDownTime * Time.deltaTime;
-        }
-    }
-
-    void ButtonClicked()
-    {
-        Shoot();
-    }
-
-    private void Shoot()
+    public void Shoot()
     {
         if (bulletAmount > 0 && timeStamp <= Time.time)
         {
             Bullet bullet = Instantiate(bulletPrefab, gun.transform.position, Quaternion.identity);
             bullet.transform.parent = gun.transform;
             bulletAmount--;
-            bulletAmountText.text = bulletAmount.ToString();
+            GetComponent<GunUi>().UpdateText();
             timeStamp = Time.time + timeBetweenShots;
 
             if (bulletAmount <= 0)
@@ -61,17 +38,11 @@ public class Gun : MonoBehaviour
 
     IEnumerator CoolDown()
     {
-        shootButton.interactable = false;
-        coolDownImage.fillAmount = 1.0f;
-        coolingDown = true;
+        GetComponent<GunUi>().shootButton.interactable = false;
+        GetComponent<GunUi>().CoolDown();
         yield return new WaitForSeconds(coolDownTime);
-        shootButton.interactable = true;
+        GetComponent<GunUi>().shootButton.interactable = true;
         bulletAmount = bulletAmountFull;
-        bulletAmountText.text = bulletAmount.ToString();
-    }
-
-    void ButtonPressed()
-    {
-
+        GetComponent<GunUi>().UpdateText();
     }
 }
---
+++ What a duck!/Assets/Scripts/GunUi.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GunUi : MonoBehaviour
+{
+    [SerializeField] Text bulletAmountText;
+    [SerializeField] Image coolDownImage;
+
+    Gun gun;
+    public Button shootButton;
+
+    void Start()
+    {
+        gun = GetComponent<Gun>();
+        coolDownImage.fillAmount = 0f;
+        shootButton = GetComponent<Button>();
+        shootButton.onClick.AddListener(ButtonClicked);
+        UpdateText();
+    }
+
+    void Update()
+    {
+        if (gun.coolingDown)
+        {
+            coolDownImage.fillAmount -= 1.0f / gun.coolDownTime * Time.deltaTime;
+        }
+    }
+    void ButtonClicked()
+    {
+        gun.Shoot();
+    }
+
+    public void UpdateText()
+    {
+        bulletAmountText.text = gun.bulletAmount.ToString();
+    }
+
+    public void CoolDown()
+    {
+        coolDownImage.fillAmount = 1.0f;
+        gun.coolingDown = true;
+    }
+}
